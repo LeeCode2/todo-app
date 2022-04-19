a8b601d78c7e1ab8425298085607628e24f547fc
Lee <1777code@gmail.com>
2022-04-18 23:15:17 -0500

    Add authorized to user

--- routes/auth.js
+++ routes/auth.js
@@ -3,15 +3,17 @@ const router = express.Router();
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const validateRegisterInput = require("../validation/registerValidation");
+const jwt = require("jsonwebtoken");
+const requireAuth = require("../middleware/permissions");
 
-// @route  Get/api/auth/test
+// @route  GET/api/auth/test
 //@desc    Test the auth route
 //@access  Is public
 router.get("/test", (req, res) => {
   res.send("Auth Router is good");
 });
 
-// @route  Post/api/auth/register
+// @route  POST/api/auth/register
 //@desc    Create a new user
 //@access  Is public
 
@@ -42,13 +44,74 @@ router.post("/register", async (req, res) => {
 
     // Save user in database
     const savedUser = await newUser.save();
-
+    const userToReturn = { ...savedUser._doc };
+    delete userToReturn.password;
     //Return the new user here
-    return res.json(savedUser);
+    return res.json(userToReturn);
   } catch (err) {
     //Catch error here
     console.log(err);
     res.status(500).send(err.message);
   }
 });
+
+// @route  POST/api/auth/login
+//@desc    Login user and return a access token
+//@access  Is public
+
+router.post("/login", async (req, res) => {
+  try {
+    //Check for user
+    const user = await User.findOne({
+      email: new RegExp("^" + req.body.email + "$", "i"),
+    });
+    if (!user) {
+      return res.status(400).json({ error: "Check email or password " });
+    }
+
+    const passwordMatch = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+
+    if (!passwordMatch) {
+      return res.status(400).json({ error: "Check email or password " });
+    }
+
+    const payload = { userId: user._id };
+
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("access-token", token, {
+      expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+    });
+
+    const userToReturn = { ...user._doc };
+    delete userToReturn.password;
+
+    return res.json({
+      token: token,
+      user: userToReturn,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send(err.message);
+  }
+});
+
+// @route  GET/api/auth/current
+//@desc    Return the currently authed user
+//@access  Is Private
+router.get("/current", requireAuth, (req, res) => {
+  if (!req.user) {
+    return res.status(401).send("Unauthorized");
+  }
+
+  return res.json(req.user);
+});
+
 module.exports = router;
